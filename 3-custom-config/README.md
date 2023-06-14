a9812fcc357f519cae199f043c895632f7c73da6
Santiago Ruiz <santiago.sruiz@mercadolibre.com.co>
2023-06-13 23:17:29 -0500

    custom config - entry, output, alias

---
+++ 3-custom-config/README.md
@@ -0,0 +1,6 @@
+
+# Webpack Custom Config
+
+Ejemplo basado en:
+
+https://github.com/davidjoy/webpack-5-examples/tree/master/03-custom-config
---
+++ 3-custom-config/src/index.js
@@ -0,0 +1,6 @@
+// require('./functions');
+// const { sum } = require('functions-module');
+
+console.log('Hello, and welcome to webpack-5-examples..');
+
+// console.log('Suma de 1 + 1 = ', sum(1, 1))
